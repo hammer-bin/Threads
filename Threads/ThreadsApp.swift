1e28d1c022d39497f05d60b4b6401603e48b771b
이민규 <mctlmk@gmail.com>
2023-09-04 05:36:16 +0900

    Initial Commit

---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by minkyuLee on 2023/09/04.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
